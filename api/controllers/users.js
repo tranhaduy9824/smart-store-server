0912297fe4e7103af5e854673a48f06c2ac4967a
tranhaduy204 <tranhaduy204@gmail.com>
2024-05-23 23:59:16 +0700

    User sign up

---
+++ api/controllers/users.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+exports.users_signup = (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Mail exists',
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            fullname: req.body.fullname,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then((result) => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch((err) => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            })
+                    }
+                })
+            }
+        })
+}
